ede73ce029169dfb2020ca51d898ad800e25e149
Henrique Franco <henriqfvr@gmail.com>
2024-09-30 09:25:40 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const infoBtn = document.getElementById('infoBtn');
+const flag = document.getElementById('flag');
+const countryName = document.getElementById('name');
+const countryCapital = document.getElementById('capital');
+const countryLanguage = document.getElementById('language');
+const countryPopulation = document.getElementById('population');
+
+function getCountryInfo(url){
+    fetch(url).then(response => {
+        return response.json();
+    }).then(data => {
+        console.log(data[0])
+        flag.src = data[0].flags.png;
+        countryName.textContent = data[0].name.common;
+        countryCapital.textContent = data[0].capital;
+        countryLanguage.textContent = Object.values(data[0].languages)[0];
+        countryPopulation.textContent = data[0].population.toLocaleString();
+    })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    getCountryInfo('https://restcountries.com/v3.1/name/Brazil');
+})
+
+infoBtn.addEventListener('click', () => {
+    const name = document.getElementById('country').value;
+    const url = `https://restcountries.com/v3.1/name/${name}`
+
+    getCountryInfo(url);
+})
